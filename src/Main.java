3fac75063db487f428a2b60334b1fb0acdd98020
whaugutin <whitchyaugustin@gmail.com>
2025-11-27 16:33:54 -0500

    Adding static method in main to display areas and perimeters

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,28 @@
 public class Main {
 
+    public static void displayArea(Quadrilateral quadrilateral){
+        System.out.println("La surface du quadrilatère 1 un est : " + quadrilateral.getArea() + " unité d'aire");
+    }
+
+    public static void displayPerimeter(Quadrilateral quadrilateral){
+        System.out.println("Le perimètre du quadrilatère 1 un est : " + quadrilateral.getPerimeter() + " unité de longueur");
+    }
 
     public static void main(String[] args) {
         System.out.println("\n\tHello World I'm Whitchy AUGUSTIN\n");
 
-        Quadrilateral quadrilateral = new Quadrilateral();
+        Quadrilateral quadrilateral1 = new Quadrilateral();
         Quadrilateral quadrilateral2 = new Quadrilateral(4, 8);
 
-        System.out.println("La surface du quadrilatère 1 un est : " + quadrilateral.getArea() + " unité d'aire");
-        System.out.println("Le perimètre du quadrilatère 1 un est : " + quadrilateral.getPerimeter() + " unité de longueur");
-        System.out.println("La surface du quadrilatère 2 un est : " + quadrilateral2.getArea() + " unité d'aire");
-        System.out.println("Le perimètre du quadrilatère 2 un est : " + quadrilateral2.getPerimeter() + " unité de longueur");
+        displayArea(quadrilateral1);
+        displayPerimeter(quadrilateral1);
+        displayArea(quadrilateral2);
+        displayPerimeter(quadrilateral2);
 
+//        System.out.println("La surface du quadrilatère 1 un est : " + quadrilateral1.getArea() + " unité d'aire");
+//        System.out.println("Le perimètre du quadrilatère 1 un est : " + quadrilateral1.getPerimeter() + " unité de longueur");
+//        System.out.println("La surface du quadrilatère 2 un est : " + quadrilateral2.getArea() + " unité d'aire");
+//        System.out.println("Le perimètre du quadrilatère 2 un est : " + quadrilateral2.getPerimeter() + " unité de longueur");
     }
+
 }
